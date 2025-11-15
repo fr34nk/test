52b32a1d9a6c735649c27c93ea44fb115418a249
fr34nk <40477970+fr34nk@users.noreply.github.com>
2025-11-15 09:28:04 +0000

    feature: add main script python

---
+++ main.py
@@ -0,0 +1,17 @@
+# !#python3 -m pip install beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://www.linkedin.com")
+soup = BeautifulSoup(response.content, 'html.parser')
+
+pretty = soup.prettify()
+print(pretty)
+
+
+headers = response.headers
+print(headers)
+
+
+
+
