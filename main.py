5452895a5b2113fb59e90b65f8a605826b1c7c3b
fr34nk <40477970+fr34nk@users.noreply.github.com>
2025-11-15 11:57:08 +0000

    env test

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+GITHUB_GIST_TOKEN=os.env["GIST_TOKEN"]
+print("GITHUB_GIST_TOKEN")
+if len(GITHUB_GIST_TOKEN) > 2:
+    print(GITHUB_GIST_TOKEN[0:2])
+
+
 response = requests.get("https://www.linkedin.com")
 soup = BeautifulSoup(response.content, 'html.parser')
 
